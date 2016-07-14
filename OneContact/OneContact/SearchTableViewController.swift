79038d199bb42c4c390dac587869e905e31329a2
iero <iero@ieroAir.local>
2016-07-14 22:53:17 +0200

    initial
    initial

--- OneContact/OneContact/SearchTableViewController.swift
+++ OneContact/OneContact/SearchTableViewController.swift
@@ -7,89 +7,176 @@
 //
 
 import UIKit
+import Kanna
 
-class SearchTableViewController: UITableViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem()
+class SearchTableViewController: UITableViewController, UISearchResultsUpdating, NSXMLParserDelegate
+{
+    let appleProducts = ["Greg Fabre","Yves Le-Stunff","Laurent Castanié","David Campion"]
+    var filteredAppleProducts = [String]()
+    var resultSearchController = UISearchController()
+    
+    var xmlParser: NSXMLParser!
+    
+    struct Item {
+        let name: String
+        let phone: String
     }
-
-    override func didReceiveMemoryWarning() {
+    var items: [Item]!
+    
+    override func viewDidLoad()
+    {
+        super.viewDidLoad()
+        
+        self.resultSearchController = UISearchController(searchResultsController: nil)
+        self.resultSearchController.searchResultsUpdater = self
+        self.resultSearchController.dimsBackgroundDuringPresentation = false
+        self.resultSearchController.searchBar.sizeToFit()
+        
+        self.tableView.tableHeaderView = self.resultSearchController.searchBar
+        
+        self.tableView.reloadData()
+    }
+    
+    override func didReceiveMemoryWarning()
+    {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
     // MARK: - Table view data source
-
-    override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
-        return 0
+    
+    override func numberOfSectionsInTableView(tableView: UITableView) -> Int
+    {
+        return 1
+    }
+    
+    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int
+    {
+        if (self.resultSearchController.active)
+        {
+            return self.filteredAppleProducts.count
+        }
+        else
+        {
+            // no search
+            //return self.appleProducts.count
+            return 0
+        }
+    }
+    
+    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell
+    {
+        let cell = tableView.dequeueReusableCellWithIdentifier("cell", forIndexPath: indexPath) as UITableViewCell?
+        
+        if (self.resultSearchController.active)
+        {
+            cell!.textLabel?.text = self.filteredAppleProducts[indexPath.row]
+            
+            return cell!
+        }
+        else
+        {
+            cell!.textLabel?.text = self.appleProducts[indexPath.row]
+            
+            return cell!
+        }
+    }
+    
+    func updateSearchResultsForSearchController(searchController: UISearchController)
+    {
+        // 3 characters minimum
+        if searchController.searchBar.text?.characters.count > 2 {
+            self.filteredAppleProducts.removeAll(keepCapacity: false)
+        
+            let searchPredicate = NSPredicate(format: "SELF CONTAINS[c] %@", searchController.searchBar.text!)
+            let array = (self.appleProducts as NSArray).filteredArrayUsingPredicate(searchPredicate)
+            self.filteredAppleProducts = array as! [String]
+        
+            self.tableView.reloadData()
+            parseAgil()
+        }
+    }
+    
+    func parseAgil() {
+        //let url:NSURL = NSURL(string: "https://itunes.apple.com/search?term=jack+johnson&limit=5")!
+        let url:NSURL = NSURL(string: "http://www.iero.org/tmp/agil/original.html")!
+        let session = NSURLSession.sharedSession()
+        
+        let request = NSMutableURLRequest(URL: url)
+        request.HTTPMethod = "POST"
+        //request.HTTPMethod = "GET"
+        request.cachePolicy = NSURLRequestCachePolicy.ReloadIgnoringCacheData
+        
+        let paramString = "nom=fab"
+        request.HTTPBody = paramString.dataUsingEncoding(NSUTF8StringEncoding)
+        
+        let task = session.dataTaskWithRequest(request) {
+            (
+            let data, let response, let error) in
+            
+            guard let _:NSData = data, let _:NSURLResponse = response  where error == nil else {
+                print("error")
+                return
+            }
+            let dataString = NSString(data: data!, encoding: NSUTF8StringEncoding)
+            //print(dataString)
+            
+            let html = String(dataString)
+    
+            if let doc = Kanna.HTML(html: html, encoding: NSUTF8StringEncoding) {
+                
+                for node in doc.xpath("//div/div/div/div[contains(@class,'personfield')] | //div/div/div/div/a[contains(@onclick,'personneSheet')]") {
+                    
+                    if (node["onclick"] != nil) {
+                        let strnode = node["onclick"]
+                        //print(strnode)
+                        let range = strnode!.startIndex.advancedBy(15) ..< strnode!.startIndex.advancedBy(23)
+                        let igg = strnode!.substringWithRange(range)
+                        print("[IGG]"+igg)
+                    } else {
+                        let s = self.condenseWhitespace(node.text!)
+                        if (!s.isEmpty) {
+                            print("["+node.className!+"]"+s)
+                        }
+                    }
+                
+                }
+            
+                //a[contains(@onClick,'personneSheet')]
+                /*for node in doc.xpath("//div/div/div[contains(@class,'personfield')] | div/a") {
+                    //if (node.className == "personfield1"){
+                    print(node.text)
+                    //print(node["onClick"])
+                    //}
+                    
+                    let s = self.condenseWhitespace(node.text!)
+                    if (!s.isEmpty) {
+                        
+                        print("["+node.className!+"]"+s)
+                    }
+                }*/
+            }
+            
+        }
+        task.resume()
+    }
+    
+    
+    func condenseWhitespace(string: String) -> String {
+        let components = string.componentsSeparatedByCharactersInSet(NSCharacterSet.whitespaceCharacterSet())
+        let filtered = components.filter({!$0.isEmpty})
+        var result = filtered.joinWithSeparator(" ")
+        result = result.stringByReplacingOccurrencesOfString("\"", withString: "", options: NSStringCompareOptions.LiteralSearch, range: nil)
+        result = result.stringByReplacingOccurrencesOfString("\t", withString: "", options: NSStringCompareOptions.LiteralSearch, range: nil)
+        result = result.stringByReplacingOccurrencesOfString("\n", withString: "", options: NSStringCompareOptions.LiteralSearch, range: nil)
+        result = result.stringByTrimmingCharactersInSet(NSCharacterSet.whitespaceAndNewlineCharacterSet())
+        if (result == ".") {
+            return ""
+        }
+        else {
+         return result
+        }
     }
 
-    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
-        return 0
-    }
 
-    /*
-    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCellWithIdentifier("reuseIdentifier", forIndexPath: indexPath)
-
-        // Configure the cell...
-
-        return cell
-    }
-    */
-
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(tableView: UITableView, canEditRowAtIndexPath indexPath: NSIndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
-    }
-    */
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(tableView: UITableView, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath) {
-        if editingStyle == .Delete {
-            // Delete the row from the data source
-            tableView.deleteRowsAtIndexPaths([indexPath], withRowAnimation: .Fade)
-        } else if editingStyle == .Insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
-    }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(tableView: UITableView, moveRowAtIndexPath fromIndexPath: NSIndexPath, toIndexPath: NSIndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(tableView: UITableView, canMoveRowAtIndexPath indexPath: NSIndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
-}
+   }
